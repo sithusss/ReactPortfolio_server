899276924b473bf9742c28dbbb7131509fed7f5b
Sandali Sithumani <sithumanisandali@gmail.com>
2025-05-25 11:57:20 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ import { connect } from 'mongoose';
 const connectDB = async () => {
   try {
     await connect(process.env.MONGO_URI, {
-      //useNewUrlParser: true,
-      //useUnifiedTopology: true, //comment karala thibbe
+      useNewUrlParser: true,
+      useUnifiedTopology: true, //comment karala thibbe
     });
     console.log('MongoDB Connected');
   } catch (err) {
